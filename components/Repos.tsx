bf05b292f843f6cf83c837af362d1e7b56729c9b
Pranam Gurung <pranamgurung@gmail.com>
2019-03-13 06:31:31 -0500

    Moved ajax call inside useEffect hook

--- components/Repos.tsx
+++ components/Repos.tsx
@@ -9,34 +9,35 @@ interface IRepo {
 
 export function Repos() {
   const [repos, setRepos] = useState([] as IRepo[]);
-  const repoUrl = 'https://api.github.com/users/pgurung/repos';
-
-  const fetchRepos = async (url: string) => {
-    const allRepos: IRepo[] = [];
-
-    await fetch(url)
-      .then(resp => resp.json())
-      .then(data => {
-        data.map((repo: any) => {
-          if (data != null) {
-            let id = repo.id;
-            let title = repo.name;
-            let url = repo.html_url;
-            let desc = repo.description;
-
-            let entry: IRepo = { id, title, url, desc };
-
-            allRepos.push(entry);
-          }
-        });
-      });
-
-    await setRepos(allRepos);
-  };
 
   useEffect(() => {
+    const repoUrl = 'https://api.github.com/users/pgurung/repos';
+
+    async function fetchRepos(url: string) {
+      const allRepos: IRepo[] = [];
+
+      await fetch(url)
+        .then(resp => resp.json())
+        .then(data => {
+          data.map((repo: any) => {
+            if (data != null) {
+              let id = repo.id;
+              let title = repo.name;
+              let url = repo.html_url;
+              let desc = repo.description;
+
+              let entry: IRepo = { id, title, url, desc };
+
+              allRepos.push(entry);
+            }
+          });
+        });
+
+      await setRepos(allRepos);
+    }
+
     fetchRepos(repoUrl);
-  }, [repoUrl]);
+  }, []);
 
   return (
     <ol>
